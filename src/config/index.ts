d6ea55979e69358038e9e29de4f4c4504ee9aeb6
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2024-02-04 23:46:21 -0400

    fix: secret

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,5 @@ type Config = {
 
 export const config: Config = {
   port: (process.env.PORT && +process.env.PORT) || 7654,
-  tokenSecret:
-    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw3hUuKyf6MII8nxkqXzR1wmnX7tRhzlOAUYa2mQDvurd/pV+0j5NP8wlHCGhfimWcrYjU7aK5g/y447kKz6/CYTefISi9DxtPO+EbGGv8qjwMWt8VXW3Z2tGpFSAHvUwNNPwr9NitIt/MDxw+l3p/4p2bEnF2+aYYfn78SwqBCERjspCDiCbwGTkFlvvew6f0MyWFJ3JSZZgvoMeY5CFpUNxyTaXVjeJVWUfWP3rp4L+JcI1KDaWymcRqHE7IOscCs4wJzfR6WuaNrq347+B6xGxr7rXRLuBfbHLVWmrAevUhyRl+FVvMzde3o4gfrBREsfF1/0PTcSCnplDnYs8rQIDAQAB',
+  tokenSecret: 'mySecretKey',
 };
